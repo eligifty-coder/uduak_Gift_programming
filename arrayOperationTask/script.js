287ab4f73bea3e8d62b8e673b38ce2f5c1e180a8
Gift <gift.weike@gmail.com>
2020-10-12 15:31:16 +0100

    updated the code

--- arrayOperationTask/script.js
+++ arrayOperationTask/script.js
@@ -1,33 +1,22 @@
 // reverse
-const reverseFun = (data)=>{
-   let reverserArr = []
-   let datalength= data.length
-   for(let i=0;i<datalength;i++){
-      let poppedItem = data.pop()
-      reverserArr.push(poppedItem)
-   }
-   return reverserArr
-}
+const reverseFun = (data) => {
+	let reverserArr = [];
+	let datalength = data.length;
+	for (let i = 0; i < datalength; i++) {
+		let poppedItem = data.pop();
+		reverserArr.push(poppedItem);
+	}
+	return reverserArr;
+};
 // multiply by 5
-const multiplyFun = (data)=>{
-   data = data.map((item)=>{
-      return item*5
-   })
-   return data
-}
+const multiplyByFiveFunc = (data) => data.map((item) => item * 5);
 // replace negative item with zero
-const replaceFun = (data)=>{
-   data = data.map((item)=>{
-      item = (item>0) ? item : 0
-      return item
-   })
-   return data
-}
-const arrayOperationFun =(input)=>{
-   const reversedInput = reverseFun(input)
-   const multipliedItem = multiplyFun(reversedInput)
-   const replaceditem = replaceFun(multipliedItem)
-   return replaceditem
-}
-const arrayInput = [2,4,-5,8]
+const replaceFun = (data) => data.map((item) => (item > 0 ? item : 0));
+const arrayOperationFun = (input) => {
+	const reversedInput = reverseFun(input);
+	const multipliedItem = multiplyByFiveFunc(reversedInput);
+	const replaceditem = replaceFun(multipliedItem);
+	return replaceditem;
+};
+const arrayInput = [2, 4, -5, 8];
 console.log(arrayOperationFun([...arrayInput]));
